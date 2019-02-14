33a33d1d1b63f7f3121e070a9e3b768ec5e0e6c6
Arron Zhu <1274294199@qq.com>
2019-02-14 17:44:22 +0800

    332

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import {
+  AddressEdit
+} from 'vant';
+import {
+  Area
+} from 'vant';
+import {
+  Toast
+} from 'vant';
 
+Vue.use(Toast);
+Vue.use(AddressEdit);
 Vue.config.productionTip = false
+Vue.use(Vant);
+Vue.use(AddressEdit);
 
+
+Vue.use(Area);
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/address/addAddress.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <van-address-edit
+      :area-list="areaList"
+      show-postal
+      show-delete
+      show-set-default
+      show-search-result
+      :search-result="searchResult"
+      @save="onSave"
+      @delete="onDelete"
+      v-model="addressData"
+    />
+
+  </div>
+</template>
+<script>
+import AreaList from './Area';
+export default {
+
+  name: 'add-adress',
+  data() {
+    return {
+      areaList: AreaList,
+      searchResult: [],
+      addressData: {}
+    }
+  },
+  mounted: {
+
+  },
+  methods: {
+    onSave(value) {
+      this.addressData = value
+    },
+    onDelete(value) {
+      value == {}
+      this.$toast('删除');
+
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
