176e01f638324eb0074aea3bfc43b9724a8ab447
Arron Zhu <1274294199@qq.com>
2019-02-28 11:13:09 +0800

    购买成功页面

--- src/views/buy/buySuccessful.vue
+++ src/views/buy/buySuccessful.vue
@@ -6,16 +6,28 @@
       </div>
     </div>
     <div class="content">
-      <div>
+      <div class="content-row">
         您的订单号为：
       </div>
-      <div>12121212121212221</div>
-      <div>
-        <van-button type="default">默认按钮</van-button>
+      <div class="content-row">12121212121212221</div>
+      <div class="content-row">
+        <van-button
+          class="buttonSubmint"
+          type="default"
+        >复制单号</van-button>
       </div>
-      <div>12121212121212221</div>
-      <div></div>
-      <div></div>
+      <div class="content-row">
+        <div class="note">
+          注：此订单号作为交易凭证，请出示于店家进行确认并付款。</div>
+      </div>
+      <div class="qrcode-area">
+        <div class="qrcode">
+
+        </div>
+      </div>
+      <div class="bottom-note">
+        长按二维码，添加店家为好友进行付款
+      </div>
     </div>
   </div>
 </template>
@@ -35,20 +47,54 @@ export default {
 <style scoped>
 .buy-wrapper {
   height: 100%;
-  background-color: darkgray;
+  background-color: #e5e5e5;
   overflow: hidden;
 }
 .card-top {
   height: 20vh;
-  background-color: antiquewhite;
+  background: url("../../assets/images/buy_successful_bg.png");
+  background-size: 100% 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  font-weight: bold;
 }
 .content {
   height: 100%;
-  margin-top: 3vh;
+  margin-top: 1.5vh;
   background-color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.content-row {
+  margin: 1.5vh 0;
+}
+.buttonSubmint {
+  color: white;
+  width: 40vw;
+  background: #f1be37;
+  border-radius: 4px;
+}
+.note {
+  font-size: 12px;
+  color: #999999;
+  letter-spacing: 0;
+}
+.bottom-note {
+  font-size: 14px;
+  color: #333333;
+}
+.qrcode-area {
+  display: flex;
+  justify-content: center;
+  margin: 1.5vh 0;
+}
+.qrcode {
+  width: 40vw;
+  height: 40vw;
+  background-color: bisque;
 }
 </style>
 
