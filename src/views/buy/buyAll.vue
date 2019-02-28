7365ae59b21e98bb4e31e3ca0923f5e01ab32b6a
Arron Zhu <1274294199@qq.com>
2019-02-28 15:15:47 +0800

    单选

--- src/views/buy/buyAll.vue
+++ src/views/buy/buyAll.vue
@@ -12,25 +12,53 @@
               <span>电话：<span v-text="curAddress.tel"></span></span>
             </div>
           </div>
-          <div class="top-part" v-text="curAddress.addressDetail"></div>
+          <div
+            class="top-part"
+            v-text="curAddress.addressDetail"
+          ></div>
         </div>
-        <div class="arrow-right" @click="goAddressList">
+        <div
+          class="arrow-right"
+          @click="goAddressList"
+        >
           <van-icon name="arrow" />
         </div>
       </div>
     </div>
     <div class="goods">
-      <van-card v-for="(item,index) in orderList" :key="item.goodsid"
-        :num="item.total"
-        :price="item.marketprice"
-        :title="item.title"
-        :thumb="item.thumb"
-      >
-        <div slot="footer">
-          <van-button size="mini" class="decrease" @click="decrease(index)">减少</van-button>
-          <van-button size="mini" class=" increase" @click="add(index)">增加</van-button>
+      <van-checkbox-group v-model="result">
+        <div
+          class="goods-check"
+          v-for="(item,index) in orderList"
+          :key="index"
+        >
+          <van-checkbox
+            :key="index"
+            :name="index"
+            checked-color="#07c160"
+          ></van-checkbox>
+          <van-card
+            :num="item.total"
+            :price="item.marketprice"
+            :title="item.title"
+            :thumb="item.thumb"
+          >
+
+            <div slot="footer">
+              <van-button
+                size="mini"
+                class="decrease"
+                @click="decrease(index)"
+              >减少</van-button>
+              <van-button
+                size="mini"
+                class=" increase"
+                @click="add(index)"
+              >增加</van-button>
+            </div>
+          </van-card>
         </div>
-      </van-card>
+      </van-checkbox-group>
     </div>
     <van-submit-bar
       :price="totalPrice"
@@ -44,18 +72,20 @@ export default {
   name: 'buyAll',
   data() {
     return {
-      curAddress:'',
-      orderList:[],
-      goodsTotal:0,
-      totalPrice:0,
-      checked:true
+      curAddress: '',
+      orderList: [],
+      goodsTotal: 0,
+      totalPrice: 0,
+      checked: true,
+      result: []
     }
   },
-  mounted(){
+  mounted() {
     this.getOrderList();
+
   },
   methods: {
-    getOrderList(){
+    getOrderList() {
       let self = this;
       self.$http.get('https://icampaign.com.cn/gomineWechat/app/index.php', {
         params: {
@@ -71,21 +101,21 @@ export default {
         self.curAddress = response.data.result.address;
       })
     },
-    decrease(index){
-      if(this.orderList[index].total >0){
-        this.orderList[index].total --
-      }else {
+    decrease(index) {
+      if (this.orderList[index].total > 0) {
+        this.orderList[index].total--
+      } else {
         this.$toast('不能再少了');
       }
     },
-    add(index){
-      this.orderList[index].total ++
+    add(index) {
+      this.orderList[index].total++
     },
     onSubmit() {
-
+      console.log(this.result)
     },
-    goAddressList(){
-      this.$router.push({ name: 'addresslist'});
+    goAddressList() {
+      this.$router.push({ name: 'addresslist' });
     }
   }
 }
@@ -167,5 +197,23 @@ export default {
 .increase {
   background-color: #ee5603;
   color: white;
+}
+.van-card__thumb img {
+  border: 0;
+  max-width: 100%;
+
+  height: 100%;
+}
+.goods-check {
+  display: flex;
+  align-items: center;
+  background-color: #fafafa;
+  margin: 1vh 0;
+}
+.van-card {
+  width: 100%;
+}
+.van-checkbox {
+  width: 32px;
 }
 </style>
